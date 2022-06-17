da714563b549d862de6b20dd290c77fef9213adb
Prem singh <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-17 14:18:07 +0530

    Update find_out_greatest.java

--- find_out_greatest.java
+++ find_out_greatest.java
@@ -17,19 +17,19 @@ public class find_out_greatest {
     	System.out.print("Please enter the  size of array : "); // get the input from user by help of scanner
     	size=sc.nextInt();
         
-    	System.out.print("Please enter the  numbers : ");
-        int[] arr = new int[size];
+    	System.out.print("Please enter the  numbers : ");// get the input from user by help of scanner
+        int[] arr = new int[size];       //using array and defined length of array
         for(int i=0; i<arr.length;i++)
         {  
-            arr[i] = sc.nextInt();
+            arr[i] = sc.nextInt();  
         }
-        int max = arr[0];
+        int max = arr[0];  
          for (int i = 1; i < arr.length; i++)
          {
                 if (arr[i] > max)
                  max = arr[i];
          }
-         System.out.println("The greatest number is :  "+max);
+         System.out.println("The greatest number is :  "+max);  //print the greatetst value 
      }
       
 
