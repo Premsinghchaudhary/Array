8b43e7e1a98005a8f98c663214d49a8d33d592a7
Prem singh <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-17 13:49:56 +0530

    Update array_use.java

--- array_use.java
+++ array_use.java
@@ -3,14 +3,12 @@ package array;
 public class array_use {
 
 public static void main(String[] args) {
-	// TODO Auto-generated method stub
-	
-	Arr_demo1 obj=new Arr_demo1();
-	//obj.ascending();
-	//obj.descending();
-	//obj.greater();
-	//obj.smaller();
-	 obj.average();
+        Arr_demo1 obj=new Arr_demo1();
+	obj.ascending();                 // call the method for print the output
+	obj.descending();
+	obj.greater();
+	obj.smaller();
+	obj.average();
 	obj.accepting_array();
 	
 }
