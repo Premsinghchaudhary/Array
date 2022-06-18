f6a9348661de62f3b62da794b645e021931d6886
Prem singh <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-18 10:45:10 +0530

    Update Assending.java

--- Assending.java
+++ Assending.java
@@ -1,29 +1,31 @@
 package array;
 import java.util.*;
-public class Assending {
+public class Assending 
+{
+         public static void main(String[] args)//main method 
+     {
+        assending(); //calling for print 
+     }
 
-
-
-
-    public static void assending()
+    public static void assending() //make the nw method by help of function
     {
         
-        int[] arr = new int[10];
-        int temp=0;
+        int[] arr = new int[10];//uisng array and set the lenght of array
+        int temp=0;//declared variable
         
-        System.out.print("Please enter the 10 values : ");
+        System.out.print("Please enter the 10 values : "); //get the inpuut from user 
         
-        for(int i=0; i<arr.length;i++)
+        for(int i=0; i<arr.length;i++) //uisng for loop 
         {
-            Scanner sc = new Scanner(System.in);
-            arr[i] = sc.nextInt();
+            Scanner sc = new Scanner(System.in); //scanner object
+            arr[i] = sc.nextInt(); 
             
         }
         for(int i=0; i<arr.length; i++)
         {
             for(int j=i+1; j<arr.length; j++)
             {
-                if(arr[i]>arr[j])
+                if(arr[i]>arr[j]) //using statement 
                 {
                    temp = arr[i];
                    arr[i] = arr[j];
@@ -44,8 +46,5 @@ public class Assending {
     }    
       
      
-     public static void main(String[] args)
-     {
-        assending();
-    }
+
  }
